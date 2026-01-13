908f3d6a166ac60ee1aaa45d565300a3dde3b1b4
Aline Lima <aline.souza21@outlook.com.br>
2026-01-13 17:04:29 -0300

    add link ao button

--- src/App.tsx
+++ src/App.tsx
@@ -45,7 +45,7 @@ export default function App() {
             </ul>
             {/* Botão */}
             <a
-              href="https://marianne-nutricionista-git-main-adsalinelima-7991s-projects.vercel.app/"
+              href="https://ong-ande.vercel.app/"
               target="_blank"
               rel="noopener noreferrer"
               className="mt-auto bg-blue-400 text-white py-2 px-4 rounded hover:bg-blue-700 transition text-center"
